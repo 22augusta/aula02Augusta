d3d3eec30d730c2d31021d82d1f28264d8e2b7ad
22augusta <aguiar.maria22@gmail.com>
2021-11-10 21:11:31 -0300

    aula04-funcao

---
+++ Aula04/atividade01.js
@@ -0,0 +1,9 @@
+let vc = 150
+let qa = 5
+let tx = vc*(0,10)
+let total = vc+(0,10*vc)
+
+console.log(tx)
+console.log(total)
+console.log(total/qa)
+
---
+++ Aula04/funcoes.js
@@ -0,0 +1,174 @@
+/*function soma (x,y) {
+return x+y    
+}
+
+console.log(soma(5,10))
+
+function subtração (x,y) {
+return x-y    
+}
+    
+console.log(subtração(15,10))
+
+function multiplicação (x,y) {
+return x*y    
+}
+        
+console.log(multiplicação(5,10))
+
+function divisão (x,y) {
+return x/y    
+}
+            
+console.log(divisão(25,10))*/
+
+/*let diaDaSemana = "sabado"
+
+switch(diaDaSemana) {
+
+case "domingo":
+
+console.log("domingou")
+
+break
+
+case "sabado":
+
+console.log("sabadou")
+
+break
+
+case "sexta":
+
+console.log("sextou")
+
+break
+
+default:
+
+console.log(`${diaDaSemana} não é um dia festivo`)
+}*/
+
+/*let diaDaSemana = "domingo"
+
+switch(diaDaSemana) {
+
+case "domingo":
+
+console.log(07)
+
+break
+
+case "sabado":
+
+console.log(06)
+
+break
+
+case "sexta":
+
+
+console.log(05)
+
+
+break
+
+case "quinta":
+
+    console.log(04)
+    
+    break
+    case "quarta":
+
+        console.log(03)
+        
+        break
+
+        case "terça":
+
+            console.log(02)
+            
+            break
+            case "segunda":
+
+                console.log(01)
+                
+                break
+
+        default:
+
+}*/
+
+/*let diaDaSemana = 07
+
+switch(diaDaSemana) {
+
+case 07:
+
+console.log("domingo")
+
+break
+
+case 06:
+
+console.log("sabado")
+
+break
+
+case 05:
+
+
+console.log("sexta")
+
+
+break
+
+case 04:
+
+    console.log("quinta")
+    
+    break
+    case 03:
+
+        console.log("quarta")
+        
+        break
+
+        case 02:
+
+            console.log( "terça")
+            
+            break
+            case 01 :
+
+                console.log("segunda")
+                
+                break
+
+        default:
+
+}*/
+
+/*let frutas=2
+
+let aluno=15
+
+let merenda= frutas*aluno
+
+console.log(merenda)*/
+
+/*let frutas=25
+let aluno=5
+
+let merenda= frutas*(aluno/5)
+
+console.log(merenda)*/
+
+let frutas=15
+
+let aluno=10
+
+let merenda= frutas*(10*100)
+
+console.log(merenda)
+
